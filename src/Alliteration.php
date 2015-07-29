2461641d0db6670f24c46967a268c7bb88f3c914
chadicus <chadwickgray@gmail.com>
2015-07-29 09:57:31 -0400

    Add Alliteration Generator.
    This generator returns 2-word alliterative adjective/animal name pairs.
    
    Code updated by @nubs to match standards of the project, but otherwise
    code was written by @chadicus.
    
    Closes #5.

---
+++ src/Alliteration.php
@@ -0,0 +1,59 @@
+<?php
+namespace Nubs\RandomNameGenerator;
+
+use Cinam\Randomizer\Randomizer;
+
+/**
+ * Defines an alliterative name generator.
+ */
+class Alliteration implements Generator
+{
+    /** @type array The definition of the potential adjectives. */
+    protected $_adjectives;
+
+    /** @type array The definition of the potential nouns. */
+    protected $_nouns;
+
+    /** @type Cinam\Randomizer\Randomizer The random number generator. */
+    protected $_randomizer;
+
+    /**
+     * Initializes the Alliteration Generator with the default word lists.
+     *
+     * @api
+     * @param \Cinam\Randomizer\Randomizer $randomizer The random number generator.
+     */
+    public function __construct(Randomizer $randomizer = null)
+    {
+        $this->_randomizer = $randomizer;
+        $this->_adjectives = file(__DIR__ . '/adjectives.txt', FILE_IGNORE_NEW_LINES);
+        $this->_nouns = file(__DIR__ . '/nouns.txt', FILE_IGNORE_NEW_LINES);
+    }
+
+    /**
+     * Gets a randomly generated alliterative name.
+     *
+     * @api
+     * @return string A random alliterative name.
+     */
+    public function getName()
+    {
+        $adjective = $this->_getRandomWord($this->_adjectives);
+        $noun = $this->_getRandomWord($this->_nouns, $adjective[0]);
+
+        return ucwords("{$adjective} {$noun}");
+    }
+
+    /**
+     * Get a random word from the list of words, optionally filtering by starting letter.
+     *
+     * @param array $words An array of words to choose from.
+     * @param string $startingLetter The desired starting letter of the word.
+     * @return string The random word.
+     */
+    protected function _getRandomWord(array $words, $startingLetter = null)
+    {
+        $wordsToSearch = $startingLetter === null ? $words : preg_grep("/^{$startingLetter}/", $words);
+        return $this->_randomizer ?  $this->_randomizer->getArrayValue($wordsToSearch) : $wordsToSearch[array_rand($wordsToSearch)];
+    }
+}
---
+++ tests/AlliterationTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Nubs\RandomNameGenerator;
+
+use PHPUnit_Framework_TestCase;
+use Cinam\Randomizer\Randomizer;
+
+/**
+ * @coversDefaultClass \Nubs\RandomNameGenerator\Alliteration
+ * @covers ::<protected>
+ */
+class AlliterationTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Verify basic behavior of getName().
+     *
+     * @test
+     * @covers ::__construct
+     * @covers ::getName
+     *
+     * @return void
+     */
+    public function getNameBasic()
+    {
+        $generator = new Alliteration();
+        $parts = explode(' ', $generator->getName());
+        $this->assertSame(2, count($parts));
+        $this->assertSame($parts[0][0], $parts[1][0]);
+    }
+
+    /**
+     * Verify basic behavior of getName() with a forced random generator.
+     *
+     * @test
+     * @covers ::__construct
+     * @covers ::getName
+     *
+     * @return void
+     */
+    public function getNameForced()
+    {
+        $numberGenerator = $this->getMock('\Cinam\Randomizer\NumberGenerator', array('getInt'));
+        $numberGenerator->expects($this->exactly(2))->method('getInt')->will($this->onConsecutiveCalls(20, 5));
+        $randomizer = new Randomizer($numberGenerator);
+
+        $generator = new Alliteration($randomizer);
+        $this->assertSame('Black Beetle', $generator->getName());
+    }
+}
